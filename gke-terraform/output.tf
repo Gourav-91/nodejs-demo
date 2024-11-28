d5e8460e3dbaac7dd4e933497c9581af98c2b252
Gourav Sarkar <srk.gourav@gmail.com>
2024-11-28 13:06:48 +0000

    added terraform code

---
+++ gke-terraform/output.tf
@@ -0,0 +1,8 @@
+# outputs.tf
+output "gke_cluster_name" {
+  value = google_container_cluster.primary.name
+}
+
+output "gke_cluster_endpoint" {
+  value = google_container_cluster.primary.endpoint
+}
